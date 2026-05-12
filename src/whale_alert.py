bb33b80f39285d4c2a030816798d6a2d339d6de1
nachh07 <ignaduplaa97@gmail.com>
2026-05-12 19:55:10 -0300

    se agregan librerias de configuraciones

---
+++ src/whale_alert.py
@@ -0,0 +1,19 @@
+'''
+ 1 - request a whale alert
+ 2 - utilizar BeautifulSoup para extraer información
+ 3 - parsear la data que me devuelve el response
+ 4 - utilizar pandas para transformar la data a un dataframe
+ 5 - cargar el dataframe a un archivo csv YYYY-MM-DD  
+ 6 - subir el archivo csv a un bucket de minio
+'''
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd 
+
+
+url = "https://whale-alert.io/whales.html"
+response = requests.get(url)
+print(response)
+
+
